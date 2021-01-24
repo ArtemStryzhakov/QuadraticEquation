39fe93424b18828e148719c17a6ee3d8aeba605c
Artem Stryzhakov <strizakov00@gmail.com>
2021-01-24 14:11:39 +0200

    Аджара Гуджу

--- QuadraticEquation/QuadraticEquation.py
+++ QuadraticEquation/QuadraticEquation.py
@@ -1,40 +1,46 @@
 from tkinter import *
+import math
+import time
 
 def btn(event):
     x = table_name.get()
     x1 = table_name1.get()
     x2 = table_name2.get()
-    Label(text="Дискриминант:", fg="dark blue").grid(row=2, column=6)
-    D = int(x1)**2-4*int(x)*int(x2)
-    label1 = Label(text=D)
-    label1.grid(row=2, column=7)
+    try:
+        D = int(x1)**2-4*int(x)*int(x2)
+        Label(text="Дискриминант:", fg="dark blue").grid(row=2, column=6)
+        label1 = Label(text=D)
+        label1.grid(row=2, column=7)
 
-    if D > 0:
-        b = int(x1) - int(x1)*2
+        if D > 0:
+            b = int(x1) - int(x1)*2
 
-        x_1 = (b + D) / 2*int(x)
-        Label(text="Первый x:", fg="dark green").grid(row=4, column=7)
-        label_2 = Label(text=x_1)
-        label_2.grid(row=4, column=9)
-        x_2 = (b - D) / 2*int(x)
-        Label(text="Второй x:", fg="dark green").grid(row=5, column=7)
-        label_3 = Label(text=x_2)
-        label_3.grid(row=5, column=9)
+            x_1 = (b + math.sqrt(D)) / 2*int(x)
+            Label(text="Первый x:", fg="dark green").grid(row=4, column=7)
+            label_2 = Label(text=x_1)
+            label_2.grid(row=4, column=9)
+            x_2 = (b - math.sqrt(D)) / 2*int(x)
+            Label(text="Второй x:", fg="dark green").grid(row=5, column=7)
+            label_3 = Label(text=x_2)
+            label_3.grid(row=5, column=9)
 
-    elif D == 0:
-        b = int(x1) - int(x1)*2
+        elif D == 0:
+            b = int(x1) - int(x1)*2
 
-        x_1 = b / 2*int(x)
-        Label(text="Первый x:", fg="dark green").grid(row=4, column=7)
-        label_1 = Label(text=x_1)
-        label_1.grid(row=4, column=9)
-        Label(text="Второй x:", fg="dark green").grid(row=5, column=7)
-        label_3 = Label(text="Отсуствует.")
-        label_3.grid(row=5, column=9)
-    else:
-        Label(text="x:", fg="red").grid(row=5, column=7)
-        label_1 = Label(text="Дискриминант меньше нуля, корни отсуствуют.")
-        label_1.grid(row=5, column=8)
+            x_1 = b / 2*int(x)
+            Label(text="Первый x:", fg="dark green").grid(row=4, column=7)
+            label_1 = Label(text=x_1)
+            label_1.grid(row=4, column=9)
+            Label(text="Второй x:", fg="dark green").grid(row=5, column=7)
+            label_3 = Label(text="Отсуствует.")
+            label_3.grid(row=5, column=9)
+        else:
+            Label(text="x:", fg="red").grid(row=5, column=7)
+            label_1 = Label(text="Дискриминант меньше нуля, корни отсуствуют.")
+            label_1.grid(row=5, column=8)
+    except:
+        Dzuba = Label(text="Ошибка при введении текста.", fg="red")
+        Dzuba.grid(row=2, column=6)
 
 root = Tk()
 root.geometry("600x200")
